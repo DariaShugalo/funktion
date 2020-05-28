52f767d703ea22bd39965addb1deaaa924043679
DariaShugalo <63112552+DariaShugalo@users.noreply.github.com>
2020-05-28 21:59:39 +0200

    Update pets.py

--- pets.py
+++ pets.py
@@ -1,4 +1,4 @@
-class Animals():
+lass Animals():
 
     def __init__(self, name, weight, sound):
         self.name = name
@@ -7,7 +7,7 @@ class Animals():
 
     def feed(self):
         self.state = "Кормить"
-
+        print("кормить")
 
 class Geese(Animals):
     def pic_up_eggs(self):
@@ -18,15 +18,13 @@ class Hen(Geese, Animals):
     pass
 
 
-class сow(Animals):
+class Cow(Animals):
     def get_milk(self):
         self.state = "Доить"
 
 
-class Goat(Cow, Animals):
+class Goat( Animals):
     pass
-
-
 class Sheep(Animals):
     def shave(self):
         self.state = "Стричь"
@@ -34,9 +32,7 @@ class Sheep(Animals):
 
 class Duck(Geese, Animals):
     pass
-
-
-cow = сow(name='Манька', weight=430, sound='Му-му')
+cow = Cow(name='Манька', weight=430, sound='Му-му')
 duck = Duck(name='Кряква', weight=15, sound='Кря-кря')
 sheep1 = Sheep(name='Барашек', weight=140, sound='бе-бе')
 sheep2 = Sheep(name='Кудрявый', weight=155, sound='бе-бе')
@@ -47,6 +43,28 @@ goat2 = Goat(name='Копыта', weight=80, sound='Ме-ме')
 goose1 = Geese(name='Серый', weight=20, sound='шшшшшш')
 goose2 = Geese(name='Белый', weight=20, sound='шшшшшш')
 
+pets = [cow, duck, sheep1, sheep2, hen1, hen2, goat1, goat2, goose1, goose2]
 
 
 
+def get_min_weight(pets_list):
+    pet_name = ''
+    min_weight = 0
+    name_list=list()
+    for pet in pets_list:
+        if min_weight==0:
+            min_weight=pet.weight
+        if len(name_list)>0 and min_weight==name_list[0]:
+            name_list.append(pet.name)
+        if pet.weight < min_weight:
+            name_list = list()
+            name_list.append(pet.name)
+            min_weight = pet.weight
+
+    return name_list, min_weight
+
+
+print(get_min_weight(pets))
+
+for pet in pets:
+    pet.feed()
